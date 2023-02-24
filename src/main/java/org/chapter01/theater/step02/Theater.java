46b31687abcb50187fd2e783e9eca77f72b3f597
sangyun lee <sangyun202@gmail.com>
2023-02-24 12:05:14 +0900

    [Modify] Decoupling Theater Class and TicketSeller Class - Add TicketSeller Class sellTo Method - Move Theater Class enter Method to TicketSeller Class sellTo Method - Change TicketSeller Class TicketOffice variable Access Modifier Public to Private

--- src/main/java/org/chapter01/theater/step02/Theater.java
+++ src/main/java/org/chapter01/theater/step02/Theater.java
@@ -7,15 +7,5 @@ public class Theater {
         this.ticketSeller = ticketSeller;
     }
 
-    public void enter(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketSeller.getTicketOffice().getTickets();
-            audience.getBag().setTicket(ticket);
-        } else {
-            Ticket ticket = ticketSeller.getTicketOffice().getTickets();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
-    }
+    public void enter(Audience audience) { }
 }
--- src/main/java/org/chapter01/theater/step02/TicketSeller.java
+++ src/main/java/org/chapter01/theater/step02/TicketSeller.java
@@ -1,13 +1,21 @@
 package main.java.org.chapter01.theater.step02;
 
 public class TicketSeller {
-    public TicketOffice ticketOffice;
+    private TicketOffice ticketOffice;
 
     public TicketSeller(TicketOffice ticketOffice) {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+    public void sellTo(Audience audience) {
+        if (audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketOffice.getTickets();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketOffice.getTickets();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
     }
 }
