23eba5cda20c7384e275899af03f8fb430a2cb61
sangyun lee <sangyun202@gmail.com>
2023-02-24 11:55:25 +0900

    [Init] Theater Object Step02

---
+++ src/main/java/org/chapter01/theater/step02/Ticket.java
@@ -0,0 +1,7 @@
+package main.java.org.chapter01.theater.step02;
+
+public class Ticket {
+    private Long fee;
+
+    public Long getFee() { return fee; }
+}
---
+++ src/main/java/org/chapter01/theater/step02/TicketSeller.java
@@ -0,0 +1,13 @@
+package main.java.org.chapter01.theater.step02;
+
+public class TicketSeller {
+    public TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
+}
