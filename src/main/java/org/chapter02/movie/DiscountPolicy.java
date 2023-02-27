1e3e3f7d95dacb407c487943fa619cf7dc56f768
sangyun lee <sangyun202@gmail.com>
2023-02-27 17:31:57 +0900

    [Add] DiscountPolicy Class - Add calculateDiscountAmount Method

---
+++ src/main/java/org/chapter02/movie/DiscountPolicy.java
@@ -0,0 +1,10 @@
+package main.java.org.chapter02.movie;
+
+import main.java.org.chapter02.money.Money;
+
+public class DiscountPolicy {
+
+    public Money calculateDiscountAmount(Screening screening) {
+        return null;
+    }
+}
