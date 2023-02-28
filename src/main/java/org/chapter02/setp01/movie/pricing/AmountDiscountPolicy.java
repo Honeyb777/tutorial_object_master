03b25da4ec3721b7e8e222cc9c2ea4f7e857052f
sangyun lee <sangyun202@gmail.com>
2023-02-28 13:59:10 +0900

    [Add] AmountDiscountPolicy implements DiscountPolicy

---
+++ src/main/java/org/chapter02/setp01/movie/pricing/AmountDiscountPolicy.java
@@ -0,0 +1,20 @@
+package main.java.org.chapter02.setp01.movie.pricing;
+
+import main.java.org.chapter02.setp01.money.Money;
+import main.java.org.chapter02.setp01.movie.DiscountCondition;
+import main.java.org.chapter02.setp01.movie.DiscountPolicy;
+import main.java.org.chapter02.setp01.movie.Screening;
+
+public class AmountDiscountPolicy extends DiscountPolicy {
+    private Money discountAmount;
+
+    public AmountDiscountPolicy(Money discountAmount, DiscountCondition ... conditions) {
+        super(conditions);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    protected Money getDiscountAmount(Screening screening) {
+        return discountAmount;
+    }
+}
