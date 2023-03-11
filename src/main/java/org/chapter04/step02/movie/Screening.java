b90d019401bb4990bba089afcf42c9428d7a357d
sangyun lee <sangyun202@gmail.com>
2023-03-11 21:03:04 +0900

    [Modify] Movie & Screening & ReservationAgency Class Encapsulation and Add Method - remove Movie Class setMovieType method - remove Movie Class fee & discountCondition & discountAmount & discountPercent variable getter/setter Method - add calculateAmountDiscountedFee Method in Movie Class - add calculatePercentDiscountFee Method in Movie Class - add calculateNoneDiscountFee Method in Movie Class - add isDiscountable Method in Movie Class - remove Screening Class movie & sequence & whenScreened variable getter/setter Method - add Screening Class Constructor - add calculateFee Method for movieType in Screening Class - modify reserve method of the ReservationAgency Class to call the calculateFee Method of the Screening Class and return the Reservation Class Constructor

--- src/main/java/org/chapter04/step02/movie/Screening.java
+++ src/main/java/org/chapter04/step02/movie/Screening.java
@@ -1,5 +1,7 @@
 package main.java.org.chapter04.step02.movie;
 
+import main.java.org.chapter04.step02.money.Money;
+
 import java.time.LocalDateTime;
 
 public class Screening {
@@ -7,27 +9,29 @@ public class Screening {
     private int sequence;
     private LocalDateTime whenScreened;
 
-    public Movie getMovie() {
-        return movie;
-    }
-
-    public void setMovie(Movie movie) {
+    public Screening(Movie movie, int sequence, LocalDateTime whenScreened) {
         this.movie = movie;
-    }
-
-    public LocalDateTime getWhenScreened() {
-        return whenScreened;
-    }
-
-    public void setWhenScreened(LocalDateTime whenScreened) {
+        this.sequence = sequence;
         this.whenScreened = whenScreened;
     }
 
-    public int getSequence() {
-        return sequence;
-    }
+    public Money calculateFee(int audienceCount) {
+        switch (movie.getMovieType()) {
+            case AMOUNT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculateNoneDiscountFee().times(audienceCount);
+                }
+                break;
+            case PERCENT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculatePercentDiscountFee().times(audienceCount);
+                }
+                break;
+            case NONE_DISCOUNT:
+                movie.calculateNoneDiscountFee().times(audienceCount);
+                break;
+        }
 
-    public void setSequence(int sequence) {
-        this.sequence = sequence;
+        return movie.calculateNoneDiscountFee().times(audienceCount);
     }
 }
