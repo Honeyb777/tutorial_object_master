ff0dfc53d6b894d074bec43b42cfe10b6006eea2
sangyun lee <sangyun202@gmail.com>
2023-03-11 20:10:57 +0900

    [Fix] Remove Getter and Setter Method - remove setType Method - remove dayOfWeek variable getter/setter Method - remove startTime variable getter/setter Method - remove endTime variable getter/setter Method

--- src/main/java/org/chapter04/step02/movie/DiscountCondition.java
+++ src/main/java/org/chapter04/step02/movie/DiscountCondition.java
@@ -5,51 +5,13 @@ import java.time.LocalTime;
 
 public class DiscountCondition {
     private DiscountConditionType type;
-
     private int sequence;
-
     private DayOfWeek dayOfWeek;
     private LocalTime startTime;
     private LocalTime endTime;
 
     public DiscountConditionType getType() {
         return type;
-    }
-
-    public void setType(DiscountConditionType type) {
-        this.type = type;
-    }
-
-    public DayOfWeek getDayOfWeek() {
-        return dayOfWeek;
-    }
-
-    public void setDayOfWeek(DayOfWeek dayOfWeek) {
-        this.dayOfWeek = dayOfWeek;
-    }
-
-    public LocalTime getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(LocalTime startTime) {
-        this.startTime = startTime;
-    }
-
-    public LocalTime getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(LocalTime endTime) {
-        this.endTime = endTime;
-    }
-
-    public int getSequence() {
-        return this.sequence;
-    }
-
-    public void setSequence(int sequence) {
-        this.sequence = sequence;
     }
 
     public boolean isDiscountable(DayOfWeek dayOfWeek, LocalTime time) {
