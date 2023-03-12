d3a2d03b9d1500abb0a266817b9ae40e46e6a777
sangyun lee <sangyun202@gmail.com>
2023-03-13 01:46:04 +0900

    [Init] Chapter05 Step03 - Init Project Structure

---
+++ src/main/java/org/chapter05/step03/movie/Screening.java
@@ -0,0 +1,27 @@
+package main.java.org.chapter05.step03.movie;
+
+import main.java.org.chapter05.step03.money.Money;
+
+import java.time.LocalDateTime;
+
+public class Screening {
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
+
+    public Reservation reserve(Customer customer, int audienceCount) {
+        return new Reservation(customer, this, calculateFee(audienceCount), audienceCount);
+    }
+
+    Money calculateFee(int audienceCount) {
+        return movie.calculateMovieFee(this).times(audienceCount);
+    }
+
+    public LocalDateTime getWhenScreened() {
+        return whenScreened;
+    }
+
+    public int getSequence() {
+        return sequence;
+    }
+}
---
+++ src/main/java/org/chapter05/step03/movie/SequenceCondition.java
@@ -0,0 +1,15 @@
+package main.java.org.chapter05.step03.movie;
+
+public class SequenceCondition {
+    private int sequence;
+
+    public SequenceCondition(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public boolean isSatisfiedBy(Screening screening) {
+        return sequence == screening.getSequence();
+    }
+}
+
+
